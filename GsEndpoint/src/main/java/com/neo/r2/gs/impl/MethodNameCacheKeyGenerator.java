dcadd1494415bee8884193dbcf77e4a990fdf1ce
Neoministein <acertartoni@gmail.com>
2023-05-25 22:41:18 +0200

    Improved MethodNameCacheKeyGenerator

--- GsEndpoint/src/main/java/com/neo/r2/gs/impl/MethodNameCacheKeyGenerator.java
+++ GsEndpoint/src/main/java/com/neo/r2/gs/impl/MethodNameCacheKeyGenerator.java
@@ -4,11 +4,17 @@ import com.neo.util.framework.api.cache.CacheKeyGenerator;
 import com.neo.util.framework.api.cache.spi.CompositeCacheKey;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
+/**
+ * Incorporates the method name with the parameters into the CompositeCacheKey
+ */
 public class MethodNameCacheKeyGenerator implements CacheKeyGenerator {
 
     @Override
     public Object generate(Method method, Object... methodParams) {
-        return new CompositeCacheKey(method.getName(), methodParams);
+        Object[] newArray = Arrays.copyOf(methodParams, methodParams.length + 1);
+        newArray[newArray.length -1] = method.getName();
+        return new CompositeCacheKey(newArray);
     }
 }
